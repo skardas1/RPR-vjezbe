8083bf6b5756b6134179b672e6355d43e01c8df1
Sara Kardas <skardas1etf.unsa.ba>
2023-10-24 13:57:51 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner ulaz=new Scanner (System.in);
+        System.out.println("Unesite broj: ");
+        int n=ulaz.nextInt();
+
+        System.out.println("Unesite broj: ");
+        int n1=ulaz.nextInt();
+        System.out.println("Prvi broj:  "+n);
+        System.out.println("Drugi broj: "+n1);
+
+	// write your code here
+    }
+};
